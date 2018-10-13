613f82eb476c3c7e634719a33044ce341dfe1f26
Julian Dane Plaisance <jplais8@lsu.edu>
2018-10-13 08:46:42 -0500

    Working on Raycasting, mouse clicks

---
+++ Assets/samples/PointAndClick.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointAndClick : MonoBehaviour {
+    public Camera cam;
+    public GameObject particle;
+    public Ray ray;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y), Input.mousePosition);
+            
+            if(hit.collider != null)
+            {
+                Debug.Log(hit.collider.ToString());
+            }
+        }
+	}
+}
