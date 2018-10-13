3f0f31d163ca3b965ebb13c896dd207b65a29ae4
Julian Dane Plaisance <jplais8@lsu.edu>
2018-10-12 20:47:00 -0500

    Some basic movements

--- Assets/samples/playerMovement.cs
+++ Assets/samples/playerMovement.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class playerMovement : MonoBehaviour {
         [SerializeField] private LayerMask WhatIsGround;
         //speed stats
         public float MAXSPEED, moveSpeed, xspeed;
