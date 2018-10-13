d88adb579a660f6cb2b4f9f9757688e6b14db185
Julian Dane Plaisance <jplais8@lsu.edu>
2018-10-13 10:34:11 -0500

    static raycast for testing

--- Assets/samples/PointAndClick.cs
+++ Assets/samples/PointAndClick.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PointAndClick : MonoBehaviour
 {
     public Camera cam;
+    public GameObject particle;
     // Use this for initialization
     void Start()
     {
@@ -14,18 +15,20 @@ public class PointAndClick : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.DrawRay(transform.position, Input.mousePosition, Color.green);
+        Debug.DrawRay(transform.position, 900 * Vector2.right, Color.green);
         if (Input.GetButtonDown("Fire1"))
         {
             Debug.Log("fire");
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, Input.mousePosition, 10000f);
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, 900 * Vector2.right);
+            //RaycastHit2D hit = Physics2D.Raycast(transform.position, Input.mousePosition, 10000f);
             
 
             Debug.Log(hit.ToString());
-            Debug.Log(hit.distance);
+
 
             if (hit.collider != null)
             {
+                Instantiate(particle, new Vector3(transform.position.x + hit.distance / 2, transform.position.y + hit.distance / 2, 0), new Quaternion());
                 /*switch (hit.collider.tag)
                 {
                     case "Tree":
