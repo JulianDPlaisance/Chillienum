a291b73b349b60f1fb28e1bec2e0bd2bda30bff8
Julian Dane Plaisance <jplais8@lsu.edu>
2018-10-12 20:58:46 -0500

    added some camera following

---
+++ Assets/samples/FollowPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+    public GameObject playr;
+    Transform trans;
+    Transform offset;
+	// Use this for initialization
+	void Start () {
+        offset = this.transform;
+        trans = playr.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = (offset.position + playr.transform.position);
+	}
+}
