49cbba211328db7990d59b4a674dd730a6e5b0d1
Elizabeth "Elise" Alvarado <danceswithtime@gmail.com>
2018-10-13 17:34:35 -0500

    Added shrinking mechanic, however, scaling isnt even and still need to set normal
    going to get help from mentor

--- Assets/_scripts/PointAndClick.cs
+++ Assets/_scripts/PointAndClick.cs
@@ -4,19 +4,25 @@ using UnityEngine;
 
 public class PointAndClick : MonoBehaviour
 {
+    public Vector3 shrinkScaling;
+    public Vector3 growthScaling;
     public Camera cam;
     public GameObject particle;
     public GameObject player;
     public LayerMask mask;
+
+    private Vector3 normalScale;
+    private bool isActive = false;
     // Use this for initialization
     void Start()
     {
-
+        normalScale = transform.localScale;
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         Ray ray = cam.ScreenPointToRay(Input.mousePosition);
         Debug.DrawLine(transform.position, ray.origin, Color.green);
 
@@ -26,11 +32,31 @@ public class PointAndClick : MonoBehaviour
             if (hit.collider != null)
             {
                 Debug.Log(hit.collider.name);
-                
+
                 switch (hit.collider.tag)
                 {
                     case "Tree":
-                        hit.collider.GetComponent<Rigidbody2D>().transform.localScale *= 2;
+                        hit.collider.GetComponent<Rigidbody2D>().transform.localScale *= 1.5f;
+                        break;
+                    case "Rock":
+                        hit.collider.GetComponent<Rigidbody2D>().mass /= 5;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+        else if (Input.GetButtonDown("Fire2"))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, ray.origin, 1000f, mask.value);
+            if (hit.collider != null)
+            {
+                Debug.Log(hit.collider.name);
+
+                switch (hit.collider.tag)
+                {
+                    case "Tree":
+                        hit.collider.GetComponent<Rigidbody2D>().transform.localScale*= 0.5f;
                         break;
                     case "Rock":
                         hit.collider.GetComponent<Rigidbody2D>().mass /= 5;
