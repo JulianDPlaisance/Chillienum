4ff03cc892d2294621e6ac26288f817b8ac9bf79
Julian Dane Plaisance <jplais8@lsu.edu>
2018-10-13 20:37:05 -0500

    Adding Music, withdrawing tifs

--- Assets/_scripts/playerMovement.cs
+++ Assets/_scripts/playerMovement.cs
@@ -2,130 +2,132 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerMovement : MonoBehaviour {
-        [SerializeField] private LayerMask WhatIsGround;
-        //speed stats
-        public float MAXSPEED, moveSpeed, xspeed;
-        public float jumpForce, secondJumpForce, jumpTime, secondJumpTime, jumpTimeCounter, secondJumpCounter;
+public class playerMovement : MonoBehaviour
+{
+    [SerializeField] private LayerMask WhatIsGround;
+    //speed stats
+    public float MAXSPEED, moveSpeed, xspeed;
+    public float jumpForce, secondJumpForce, jumpTime, secondJumpTime, jumpTimeCounter, secondJumpCounter;
+    public AudioClip[] clip;
 
 
-        //jump checks
-        bool grounded;
-        int TimesJumped;
-        //Rigidbody2D for player
-        private Rigidbody2D rigid2D;
-        //Checks for ground and ceiling
-        private Transform GroundCheck, CeilingCheck;
-        const float GroundedRadius = 0.2f, CeilingRadius = 0.1f;
-        //facing direction
-        public bool facingRight = true;
+    //jump checks
+    bool grounded;
+    int TimesJumped;
+    //Rigidbody2D for player
+    private Rigidbody2D rigid2D;
+    //Checks for ground and ceiling
+    private Transform GroundCheck, CeilingCheck;
+    const float GroundedRadius = 0.2f, CeilingRadius = 0.1f;
+    //facing direction
+    public bool facingRight = true;
 
-        private void Awake()
+    private void Awake()
+    {
+        rigid2D = GetComponent<Rigidbody2D>();
+        GroundCheck = transform.Find("GroundCheck");
+        CeilingCheck = transform.Find("CeilingCheck");
+    }
+
+    void Start()
+    {
+
+        jumpTimeCounter = jumpTime;
+        secondJumpCounter = secondJumpTime;
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetAxis("Horizontal") != 0.0f)
         {
-            rigid2D = GetComponent<Rigidbody2D>();
-            GroundCheck = transform.Find("GroundCheck");
-            CeilingCheck = transform.Find("CeilingCheck");
+            xspeed += Input.GetAxis("Horizontal") * (moveSpeed * (1 - Time.deltaTime));
         }
-
-        void Start()
+        else
         {
-
-            jumpTimeCounter = jumpTime;
-            secondJumpCounter = secondJumpTime;
-        }
-
-        private void FixedUpdate()
-        {
-            if (Input.GetAxis("Horizontal") != 0.0f)
+            if ((xspeed > -0.45f && xspeed < 0.0f) || (xspeed > 0.45f && xspeed > 0.0f))
             {
-                xspeed += Input.GetAxis("Horizontal") * (moveSpeed * (1 - Time.deltaTime));
+                xspeed -= xspeed * 0.45f;
             }
             else
             {
-                if ((xspeed > -0.45f && xspeed < 0.0f) || (xspeed > 0.45f && xspeed > 0.0f))
-                {
-                    xspeed -= xspeed * 0.45f;
-                }
-                else
-                {
-                    xspeed = 0;
-                }
+                xspeed = 0;
             }
-            if (Mathf.Abs(xspeed) >= MAXSPEED)
-            {
-                xspeed = Input.GetAxis("Horizontal") * MAXSPEED;
-            }
-            rigid2D.velocity = new Vector2(xspeed, rigid2D.velocity.y);
-            if (grounded == false && TimesJumped == 2)
-            {
-                rigid2D.velocity = new Vector2(rigid2D.velocity.x * 0.75f, rigid2D.velocity.y);
-            }
-
-
-            if (Input.GetButtonDown("Jump"))
-            {
-                if (grounded)
-                {
-                    rigid2D.velocity = new Vector2(rigid2D.velocity.x, jumpForce);
-                    TimesJumped = 0;
-                }
-                else if (!grounded && TimesJumped < 2)
-                {
-                    rigid2D.velocity = new Vector2(rigid2D.velocity.x, secondJumpForce);
-                }
-            }
-
-            if (Input.GetButton("Jump"))
-            {
-                if (TimesJumped == 0)
-                {
-                    if (jumpTimeCounter > 0)
-                    {
-                        rigid2D.velocity = new Vector2(rigid2D.velocity.x, jumpForce);
-                        jumpTimeCounter -= Time.deltaTime;
-                    }
-                }
-                else if (TimesJumped == 1)
-                {
-                    if (secondJumpCounter > 0)
-                    {
-                        rigid2D.velocity = new Vector2(rigid2D.velocity.x * 0.75f, secondJumpForce);
-                        secondJumpCounter -= Time.deltaTime;
-                    }
-                }
-            }
-
-
+        }
+        if (Mathf.Abs(xspeed) >= MAXSPEED)
+        {
+            xspeed = Input.GetAxis("Horizontal") * MAXSPEED;
+        }
+        rigid2D.velocity = new Vector2(xspeed, rigid2D.velocity.y);
+        if (grounded == false && TimesJumped == 2)
+        {
+            rigid2D.velocity = new Vector2(rigid2D.velocity.x * 0.75f, rigid2D.velocity.y);
         }
 
-        // Update is called once per frame
-        void Update()
-        {
-            grounded = Physics2D.OverlapCircle(GroundCheck.position, GroundedRadius, WhatIsGround);
 
+        if (Input.GetButtonDown("Jump"))
+        {
             if (grounded)
             {
-                jumpTimeCounter = jumpTime;
-                secondJumpCounter = secondJumpTime;
+                rigid2D.velocity = new Vector2(rigid2D.velocity.x, jumpForce);
                 TimesJumped = 0;
             }
-            if (Input.GetButtonUp("Jump"))
+            else if (!grounded && TimesJumped < 2)
             {
-                if (TimesJumped == 0)
+                rigid2D.velocity = new Vector2(rigid2D.velocity.x, secondJumpForce);
+            }
+        }
+
+        if (Input.GetButton("Jump"))
+        {
+            if (TimesJumped == 0)
+            {
+                if (jumpTimeCounter > 0)
                 {
-                    jumpTimeCounter = 0;
-                    TimesJumped = 1;
+                    rigid2D.velocity = new Vector2(rigid2D.velocity.x, jumpForce);
+                    jumpTimeCounter -= Time.deltaTime;
                 }
-                else if (TimesJumped == 1)
+            }
+            else if (TimesJumped == 1)
+            {
+                if (secondJumpCounter > 0)
                 {
-                    secondJumpCounter = 0;
-                    TimesJumped = 2;
+                    rigid2D.velocity = new Vector2(rigid2D.velocity.x * 0.75f, secondJumpForce);
+                    secondJumpCounter -= Time.deltaTime;
                 }
             }
         }
 
-        private void Flip()
-        {
 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        grounded = Physics2D.OverlapCircle(GroundCheck.position, GroundedRadius, WhatIsGround);
+
+        if (grounded)
+        {
+            jumpTimeCounter = jumpTime;
+            secondJumpCounter = secondJumpTime;
+            TimesJumped = 0;
+        }
+        if (Input.GetButtonUp("Jump"))
+        {
+            if (TimesJumped == 0)
+            {
+                jumpTimeCounter = 0;
+                TimesJumped = 1;
+            }
+            else if (TimesJumped == 1)
+            {
+                secondJumpCounter = 0;
+                TimesJumped = 2;
+            }
         }
     }
+
+    private void Flip()
+    {
+
+    }
+}
