79f57c19731cc82f7234878dc4f1da285839cb58
Julian Dane Plaisance <jplais8@lsu.edu>
2018-10-14 09:11:10 -0500

    hopefully fixed going into the floor

---
+++ Assets/_scripts/FinishZone.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishZone : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player" || collision.name == "Player" || collision.gameObject.name == "Player")
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+        }
+    }
+}
