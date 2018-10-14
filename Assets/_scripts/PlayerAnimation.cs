01da63bca254d30bdc9353f546a9b26c2e497701
Julian Dane Plaisance <jplais8@lsu.edu>
2018-10-14 07:42:24 -0500

    moved some art around, updated clickbaitability

---
+++ Assets/_scripts/PlayerAnimation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour {
+    public Sprite[] standing, walking, jumping, dying;
+	public void Standing()
+    {
+
+    }
+
+    public void Walking()
+    {
+
+    }
+
+    public void Jumping()
+    {
+
+    }
+
+    public void Dying()
+    {
+
+    }
+}
--- Assets/_scripts/PointAndClick.cs
+++ Assets/_scripts/PointAndClick.cs
@@ -8,10 +8,11 @@ public class PointAndClick : MonoBehaviour
     public Camera cam;
     public GameObject player;
     public LayerMask mask;
+    public UnityEngine.UI.Button truth;
     // Use this for initialization
     void Start()
     {
-       
+
     }
 
     // Update is called once per frame
@@ -19,11 +20,28 @@ public class PointAndClick : MonoBehaviour
     {
         
         Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-        Debug.DrawLine(transform.position, ray.origin, Color.green);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, ray.origin, 1000f, mask.value);
+        //Debug.DrawLine(transform.position, ray.origin, Color.green);
+
+        truth.transform.position = ray.origin;
+        if (hit.collider != null)
+        {
+            if(hit.collider.tag == "Tree" || hit.collider.tag == "Rock")
+            {
+                truth.image.color = Color.green;
+            }
+            else
+            {
+                truth.image.color = Color.red;
+            }
+        }
+        else
+        {
+            truth.image.color = Color.red;
+        }
 
         if (Input.GetButtonDown("Fire1"))
         {
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, ray.origin, 1000f, mask.value);
             if (hit.collider != null)
             {
                 Debug.Log(hit.collider.name);
@@ -49,7 +67,6 @@ public class PointAndClick : MonoBehaviour
         }
         else if (Input.GetButtonDown("Fire2"))
         {
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, ray.origin, 1000f, mask.value);
             if (hit.collider != null)
             {
                 Debug.Log(hit.collider.name);
